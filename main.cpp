92398837e7ffcbd6461b8229572516053cda5578
Gutan Mihai <mihai.g@avantajprim.com>
2023-11-10 18:13:26 +0200

    added task-1 cpp class

--- main.cpp
+++ main.cpp
@@ -1,54 +1,13 @@
 #include <iostream>
 #include "list/singly_linked.cpp"
 #include "list/doubly_linked.cpp"
+#include "practice/5/task-1.cpp"
 
 using namespace std;
 
-// compile code:
-// clang++ -std=c++20 -stdlib=libc++ -g main.cpp
-
 int main() {
-    cout << "----- SinglyLinked -----" << endl;
-    SinglyLinked<int> slst;
-    slst.push_back(2);
-    slst.push_back(3);
-    slst.push_front(4);
-    slst.push_front(1);
-    slst.insert(5, 1);
-    cout << "size: " << slst.size() << endl;
-    for (int i = 0; i < slst.size(); ++i) {
-        cout << "value: " << slst[i] << endl;
-    }
+    Practice5Task1 task;
+    task.run();
 
-    cout << "start removing" << endl;
-    slst.pop_back();
-    slst.pop_front();
-    slst.remove_at(1);
-    cout << "size: " << slst.size() << endl;
-    for (int i = 0; i < slst.size(); ++i) {
-        cout << "value: " << slst[i] << endl;
-    }
-
-    // cout << "----- DoublyLinked -----" << endl;
-    // DoublyLinked<int> dlst;
-    // dlst.push_back(2);
-    // dlst.push_back(3);
-    // dlst.push_front(4);
-    // dlst.push_front(1);
-    // dlst.insert(5, 1);
-    // cout << "size: " << dlst.size() << endl;
-    // for (int i = 0; i < dlst.size(); ++i) {
-    //     cout << "value: " << dlst[i] << endl;
-    // }
-    
-    // cout << "start removing" << endl;
-    // dlst.pop_back();
-    // dlst.pop_front();
-    // dlst.remove_at(1);
-    // cout << "size: " << dlst.size() << endl;
-    // for (int i = 0; i < dlst.size(); ++i) {
-    //     cout << "value: " << dlst[i] << endl;
-    // }
-
-    // return 0;
+    return 0;
 }
---
+++ practice/5/task-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+class Practice5Task1 {
+public:
+    void run() {
+        cout << "Hello from Practice5Task1" << endl;
+    }
+};
